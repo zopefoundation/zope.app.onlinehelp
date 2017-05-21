0cd750a10af18f630a3dd3560bbd3139a438360e
Jason Madden <jamadden@gmail.com>
2017-05-21 07:22:27 -0500

    Back to development: 4.0.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
         return f.read()
 
 setup(name='zope.app.onlinehelp',
-      version='4.0.1',
+      version='4.0.2.dev0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Framework for Context-Sensitive Help Pages',
